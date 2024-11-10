e37aa7ca8b1ae48765e880756ac6fbcf3bd93986
Dxung <dung.lx183505@gmail.com>
2024-11-10 22:43:26 +0700

    minor update

--- backend/src/routes/ProductRouter.js
+++ backend/src/routes/ProductRouter.js
@@ -8,7 +8,7 @@ const { authMiddleware } = require('../middleware/authMiddleware');
 router.post('/create', productController.createProduct);
 router.put('/update/:id', productController.updateProduct);
 router.get('/detail/:id', productController.getProductDetails);
-router.get('/detail/get-all',productController.getAllProduct);
+router.get('/get-all',productController.getAllProduct);
 router.delete('/delete/:id',authMiddleware, productController.deleteProduct);
 
 
