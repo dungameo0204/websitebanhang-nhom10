a2895800d199ba126dbe972d76529a3e914a476f
unknown <buiquangdung850@gmail.com>
2024-11-13 18:00:24 +0700

    second commit

--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import ProductDetailsComponent from "../../components/ProductDetailsComponent/ProductDetailsComponent";
 
 const ProductDetailsPage = () => {
-  return <div>ProductDetailsPage</div>;
+  return (
+    <div style={{ padding: "0 12px", background: "#efefef", height: "1000px" }}>
+      <h5>Trang chủ</h5>
+      <ProductDetailsComponent />
+    </div>
+  );
 };
 
 export default ProductDetailsPage;
