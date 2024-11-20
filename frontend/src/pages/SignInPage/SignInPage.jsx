464db0526eac8cc26daae52aa1ed647e210eb807
unknown <buiquangdung850@gmail.com>
2024-11-21 03:34:36 +0700

    SignIn & SignUp

---
+++ frontend/src/pages/SignInPage/SignInPage.jsx
@@ -0,0 +1,77 @@
+import react from "react";
+import ButtonComponent from "../../components/ButtonComponent/ButtonComponent";
+import InputForm from "../../components/InputForm/InputForm";
+import { WrapperContainerLeft, WrapperContainerRight } from "./style";
+import imageLogo from "../../assets/images/logo.png";
+import { Image } from "antd";
+
+const SignInPage = () => {
+  return (
+    <div
+      style={{
+        dissplay: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "rgba(0,0,0,0.53",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          width: "800px",
+          height: "445px",
+          borderRadius: "6px",
+          background: "#fff",
+          display: "flex",
+        }}
+      >
+        <WrapperContainerLeft>
+          <h1>Xin chào</h1>
+          <p>Đăng nhập vào tạo tài khoản</p>
+          <InputForm
+            style={{ marginBottom: "10px" }}
+            placeholder="abc@gmail.com"
+          />
+          <InputForm placeholder="Mật khẩu" />
+          <ButtonComponent
+            bordered={false}
+            size={40}
+            styleButton={{
+              background: "rgb(255, 57, 69)",
+              height: "48px",
+              width: "100%",
+              borderRadius: "4px",
+              border: "none",
+              margin: "26px 0 10px",
+            }}
+            textButton={"Đăng nhập"}
+            styleTextButton={{
+              color: "#fff",
+              fontSize: "15px",
+              fontWeight: "700",
+            }}
+          ></ButtonComponent>
+          <p>
+            <WrapperTextLight>Quên mật khẩu?</WrapperTextLight>
+          </p>
+          <p>
+            Chưa có tài khoản?{" "}
+            <WrapperTextLight> Tạo tài khoản</WrapperTextLight>
+          </p>
+        </WrapperContainerLeft>
+        <WrapperContainerRight>
+          <Image
+            src={imageLogo}
+            preview={false}
+            alt="iamge-logo"
+            height="203px"
+            width="203px"
+          />
+          <h4>Mua sắm tại ...</h4>
+        </WrapperContainerRight>
+      </div>
+    </div>
+  );
+};
+
+export default SignInPage;
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,31 +3,48 @@ import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import OderPage from "../pages/OderPage/OderPage";
 import ProductsPage from "../pages/ProductsPage/ProductsPage";
 import TypeProductPage from "../pages/TypeProductPage/TypeProductPage";
+import ProductDetailPage from "../pages/ProductDetailPage/ProductDetailPage";
+import SignInPage from "../pages/SignInPage/SignInPage";
+import SignUpPage from "../pages/SignUpPage/SignUpPage";
 
 export const routes = [
-    {
-        path: '/',
-        page: HomePage,
-        isShowHeader: true
-    },
-    {
-        path: '/order',
-        page: OderPage,
-        isShowHeader: true
-    },
-    {
-        path: '/products',
-        page: ProductsPage,
-        isShowHeader: true
-    },
-    {
-        path: '/type',
-        page: TypeProductPage,
-        isShowHeader: true
-    },
-    {
-        path: '*',
-        page: NotFoundPage
-    }
-
-]
+  {
+    path: "/",
+    page: HomePage,
+    isShowHeader: true,
+  },
+  {
+    path: "/order",
+    page: OderPage,
+    isShowHeader: true,
+  },
+  {
+    path: "/products",
+    page: ProductsPage,
+    isShowHeader: true,
+  },
+  {
+    path: "/type",
+    page: TypeProductPage,
+    isShowHeader: true,
+  },
+  {
+    path: "/signin",
+    page: SignInPage,
+    isShowHeader: false,
+  },
+  {
+    path: "/signup",
+    page: SignUpPage,
+    isShowHeader: false,
+  },
+  {
+    path: "/product-details",
+    page: ProductDetailsPage,
+    isShowHeader: true,
+  },
+  {
+    path: "*",
+    page: NotFoundPage,
+  },
+];
