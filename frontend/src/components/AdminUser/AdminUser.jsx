f6b74491f69a501df6161be998f769387039fe44
TienDung2003 <128325350+TienDung2003@users.noreply.github.com>
2024-12-16 20:02:03 +0700

    AdminUser.jsx

--- frontend/src/components/AdminUser/AdminUser.jsx
+++ frontend/src/components/AdminUser/AdminUser.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { WrapperHeader } from './style'
 import { Button } from 'antd'
-import { PlusCircleFilled } from '@ant-design/icons'
+import { PlusCircleFilled, PlusOutlined } from '@ant-design/icons'
 import TableComponent from '../TableComponent/TableComponent'
 
 const AdminUser = () => {
   return (
     <div>
       <WrapperHeader>Quản lý người dùng</WrapperHeader>
-      <Button><PlusCircleFilled /></Button>
+      <Button style={{ height: '150px', width: '150px', borderRadius: '6px', borderStyle: 'dashed' }}><PlusOutlined style={{ fontSize: '40px' }} /></Button>
       <div style={{ margin: '20px' }}>
         <TableComponent />
       </div>
